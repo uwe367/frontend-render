4416b460e4271b6419ff00b430356358df4cb21f
Uwe Tholey <uwe367@gmx.de>
2018-06-03 15:43:15 +0200

    Render Category ID. Read and follow the instructions in the script.

---
+++ render_cat_id.php
@@ -0,0 +1,17 @@
+<?php
+// -------------------------------------------------------------------------------------------
+// ------------------------------- Render Category ID ----------------------------------------
+// Instructions: 
+// Move the Script as, f.e., render_cat_id.php into template/inc_script/frontend_render
+// Extend conf.template_default.inc.php by the following line:
+// $template_default['cat_id'] = '1'; //Set '1' to render Category ID in Source Code
+// -------------------------------------------------------------------------------------------
+// obligate check for phpwcms constants
+  if (!defined('PHPWCMS_ROOT')) {die("You Cannot Access This Script Directly, Have a Nice Day.");}
+// -------------------------------------------------------------------------------------------
+
+if($template_default['cat_id']==1) {
+    $content['body_id'] = $content['cat_id'];
+}
+?>
+
